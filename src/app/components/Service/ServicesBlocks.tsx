8aa15c7132fe2391418cc4d085169db6107003d6
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-01-03 12:04:54 +0530

    first commit

--- src/app/components/Service/ServicesBlocks.tsx
+++ src/app/components/Service/ServicesBlocks.tsx
@@ -46,14 +46,14 @@ export default function ServicesBlocks() {
   return (
     <section className="py-20 px-4 lg:px-28 space-y-10 py-12 md:py-32 px-6 lg:px-28">
 
-<div className="absolute top-130 inset-0 z-[1] h-[800px]
-  bg-[radial-gradient(circle_at_50%_55%,rgba(0,255,153,0.45)_20%,transparent_50%)]
-  md:bg-[radial-gradient(circle_at_70%_45%,rgba(0,255,153,0.35)_1%,transparent_28%)] opacity-90 "
+<div className="absolute top-90 md:top-130 inset-0 z-[1]  md:h-[800px]
+  bg-[radial-gradient(circle_at_64%_53%,rgba(0,255,153,0.45)_1%,transparent_40%)]
+  md:bg-[radial-gradient(circle_at_70%_45%,rgba(0,255,153,0.35)_2%,transparent_28%)] opacity-50 md:opacity-90 "
 />
 
-<div className="absolute top-130 inset-0 z-[1] h-[800px]
-  bg-[radial-gradient(circle_at_50%_55%,rgba(0,255,153,0.45)_20%,transparent_50%)]
-  md:bg-[radial-gradient(circle_at_29%_45%,rgba(0,255,153,0.35)_1%,transparent_34%)] opacity-90 "
+<div className="absolute top-90 md:top-130 inset-0 z-[1] md:h-[800px]
+  bg-[radial-gradient(circle_at_22%_53%,rgba(0,255,153,0.45)_2%,transparent_40%)]
+  md:bg-[radial-gradient(circle_at_29%_45%,rgba(0,255,153,0.35)_1%,transparent_34%)] opacity-50 md:opacity-90 "
 />
 
 
@@ -67,9 +67,9 @@ export default function ServicesBlocks() {
           className={`service-card2 cursor-pointer p-[2.7px]`}
         >
             
-          <div className="service-card grid grid-cols-2 items-center gap-10 p-8 md:p-12  justify-left  items-center">
+          <div className="service-card flex flex-row md:grid grid-cols-2  md:gap-10 p-2 lg:p-12  items-center">
             {/* Illustration */}
-            <div className="relative lg:w-[600px] h-[420px] md:h-[360px]">
+            <div className="xl:w-[600px] w-[100px] sm:w-[400px] h-[100px] md:h-[420px] xl:h-[360px]  items-center justify-left  backdrop-blur">
               <Image
                 src={service.image}
                 alt={service.title}
@@ -80,13 +80,13 @@ export default function ServicesBlocks() {
             
 
             {/* Text */}
-            <div className="text-center space-y-4  p-8 md:p-12 z-2"
+            <div className="text-center space-y-4 w-full md:p-12 z-2 "
             onMouseMove={handleMouseMove}>
-              <div className="inline-flex items-center justify-center w-20 h-20 rounded-xl bg-white/10 backdrop-blur">
-                <service.icon className="text-gray-400 h-10 w-10" />
+              <div className="inline-flex items-center justify-center p-2 md:w-20 md:h-20 rounded-xl bg-white/10 backdrop-blur">
+                <service.icon className="text-gray-400 md:h-10 md:w-10" />
               </div>
 
-              <h3 className="text-gray-400 font-zendots tracking-wide  md:text-2xl"
+              <h3 className="text-gray-400 font-zendots tracking-wide text-[12px]  md:text-2xl"
                         style={{
             backgroundImage: `radial-gradient(at ${pos.x}% ${pos.y}%, #81f1b5 0%, #e9fffd 80%)`,
             WebkitBackgroundClip: "text",
@@ -97,7 +97,7 @@ export default function ServicesBlocks() {
                 {service.title}
               </h3>
 
-              <p className="text-gray-400  ml-auto text-sm md:text-base leading-relaxed">
+              <p className="text-gray-400  ml-auto text-[8px] md:text-base leading-relaxed">
                 {service.desc}
               </p>
             </div>
