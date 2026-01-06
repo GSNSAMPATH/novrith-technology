4de9f9beb8c1b156a3d43d705c55fd3d56d5d849
GSNSAMPATH <novrithtechnolgy@gmail.com>
2026-01-07 00:04:45 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,8 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico" },
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon.png", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
